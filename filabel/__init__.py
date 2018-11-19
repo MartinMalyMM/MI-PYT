7151a07f2e947a902775c43e066c8a9ef4e4751d
MartinMalyMM <malymar9@fjfi.cvut.cz>
2018-11-19 10:32:44 +0100

    My tests - 2nd

--- filabel/__init__.py
+++ filabel/__init__.py
@@ -1,3 +1,11 @@
 from .cli_app import main
 from .web_app import create_app
+
+# For testing reasons
+from .commons import find_labels 
+from .commons import find_pulls
+from .commons import find_pull_files
+from .commons import find_pull_labels
+
+
 __all__ = ['main', 'create_app']
